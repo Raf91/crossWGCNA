66f55a156baeda2f0426ed92f940711872190789
Aurora Savino <aurora.savino@fht.org>
2023-01-16 16:12:02 +0100

    testing the agreement between datasets with the two methods - added plots

--- scripts/06_datasets_coherence.R
+++ scripts/06_datasets_coherence.R
@@ -1,43 +1,86 @@
+library(pheatmap)
+library(ggpubr)
+save_pheatmap_pdf <- function(x, filename, width=5, height=5) {
+    stopifnot(!missing(x))
+    stopifnot(!missing(filename))
+    pdf(filename, width=width, height=height)
+    grid::grid.newpage()
+    grid::grid.draw(x$gtable)
+    dev.off()
+}
+
+
 load("results/degs_3rd_netdiff.RData")
-source("scripts/crossWGCNA_functions_netdiff.R")
-degs <-
-    list(net_GSE5847,
-         net_GSE10797,
-         net_GSE14548,
-         net_GSE83591,
-         net_GSE68744,
-         net_GSE88715)
-library(clusterProfiler)
-library(openxlsx)
+degs_netdiff<-degs
+load("results/degs_3rd_selfloops.RData")
+
+
 names(degs) <- c("GSE5847",
                  "GSE10797",
                  "GSE14548",
                  "GSE83591",
                  "GSE68744",
                  "GSE88715")
+names(degs_netdiff) <- c("GSE5847",
+                 "GSE10797",
+                 "GSE14548",
+                 "GSE83591",
+                 "GSE68744",
+                 "GSE88715")
 
-corrs <- matrix(nrow = length(degs), ncol = length(degs))
+k_type<-c("kInt_stroma", "kInt_epi", "kExt_stroma", "kExt_epi", "kTot_stroma", "kTot_epi")
+
+for(k in 1:6){
+
+corrs <- matrix(nrow = length(degs_netdiff), ncol = length(degs_netdiff))
+for (i in 1:length(degs_netdiff)) {
+    for (j in 1:length(degs_netdiff)) {
+        incommon <-
+            intersect(names(degs_netdiff[[i]][[k]]),
+                      names(degs_netdiff[[j]][[k]]))
+        corrs[i, j] <- cor(degs_netdiff[[i]][[k]][incommon], degs_netdiff[[j]][[k]][incommon])
+    }
+}
+
+
+corrs_sl <- matrix(nrow = length(degs), ncol = length(degs))
 for (i in 1:length(degs)) {
     for (j in 1:length(degs)) {
         incommon <-
-            intersect(names(degs[[i]][[1]])[grep("tis1", names(degs[[i]][[1]]))],
-                      names(degs[[j]][[1]])[grep("tis1", names(degs[[j]][[1]]))])
-        corrs[i, j] <- cor(degs[[i]][[1]][incommon], degs[[j]][[1]][incommon])
-    }
-}
-
-load("results/degs_selfloops.RData")
-
-corrs_sl <- matrix(nrow = length(degs_old), ncol = length(degs_old))
-for (i in 1:length(degs_old)) {
-    for (j in 1:length(degs_old)) {
-        incommon <-
-            intersect(names(degs_old[[i]][[1]][[1]])[grep("tis1", names(degs_old[[i]][[1]][[1]]))],
-                      names(degs_old[[j]][[1]][[1]])[grep("tis1", names(degs_old[[j]][[1]][[1]]))])
+            intersect(names(degs[[i]][[k]]),
+                      names(degs[[j]][[k]]))
         corrs_sl[i, j] <-
-            cor(degs_old[[i]][[1]][[1]][incommon], degs_old[[j]][[1]][[1]][incommon])
+            cor(degs[[i]][[k]][incommon], degs[[j]][[k]][incommon])
     }
 }
 
-hist(corrs_sl, breaks = 20)
-hist(corrs, breaks = 20)
+colnames(corrs_sl)<-names(degs)
+rownames(corrs_sl)<-names(degs)
+colnames(corrs)<-names(degs)
+rownames(corrs)<-names(degs)
+
+df<-data.frame(R=c(corrs_sl[upper.tri(corrs_sl)],corrs[upper.tri(corrs)]),
+               method=c(rep("selfloops", length=length(corrs_sl[upper.tri(corrs_sl)])),
+                                                         rep("netdiff", length=length(corrs[upper.tri(corrs)]))))
+
+pdf(paste("results/Compare_corr_",k_type[k],".pdf",sep=""), 4, 6)
+my_comparisons<-list(c("selfloops", "netdiff"))
+p<-ggviolin(df, x = "method", y = "R",
+            palette = "jco", add="boxplot") + stat_compare_means(comparisons=my_comparisons) +
+    rotate_x_text(angle = 45)
+print(p)
+dev.off()
+
+
+paletteLength <- 50
+myColor <- colorRampPalette(c("white", "red"))(paletteLength)
+myBreaks <- c(seq(0,max(unlist(corrs_sl), unlist(corrs), na.rm=T),
+              length.out=floor(paletteLength)))
+length(myBreaks) == length(paletteLength) + 1
+
+p<-pheatmap(corrs_sl,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, paste("results/corr_",k_type[k],"_selfloops.pdf", sep=""))
+
+p<-pheatmap(corrs,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, paste("results/corr_",k_type[k],"_netdiff.pdf", sep=""))
+}
