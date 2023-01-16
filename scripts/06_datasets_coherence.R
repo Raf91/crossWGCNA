8b6447874fe4025f006643ebcc165072b8d05693
Aurora Savino <aurora.savino@fht.org>
2023-01-16 16:25:55 +0100

    kRatio agreement across datasets

--- scripts/06_datasets_coherence.R
+++ scripts/06_datasets_coherence.R
@@ -84,3 +84,118 @@ save_pheatmap_pdf(p, paste("results/corr_",k_type[k],"_selfloops.pdf", sep=""))
 p<-pheatmap(corrs,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
 save_pheatmap_pdf(p, paste("results/corr_",k_type[k],"_netdiff.pdf", sep=""))
 }
+
+################
+###kRatio
+################
+
+corrs <- matrix(nrow = length(degs_netdiff), ncol = length(degs_netdiff))
+for (i in 1:length(degs_netdiff)) {
+    for (j in 1:length(degs_netdiff)) {
+        incommon <-
+            intersect(names(degs_netdiff[[i]][[3]]),
+                      names(degs_netdiff[[j]][[3]]))
+        corrs[i, j] <- cor(degs_netdiff[[i]][[3]][incommon]/degs_netdiff[[i]][[1]][incommon],
+                           degs_netdiff[[j]][[3]][incommon]/degs_netdiff[[j]][[1]][incommon])
+    }
+}
+
+
+corrs_sl <- matrix(nrow = length(degs), ncol = length(degs))
+for (i in 1:length(degs)) {
+    for (j in 1:length(degs)) {
+        incommon <-
+            intersect(names(degs[[i]][[3]]),
+                      names(degs[[j]][[3]]))
+        corrs_sl[i, j] <- cor(degs[[i]][[3]][incommon]/degs[[i]][[1]][incommon],
+                           degs[[j]][[3]][incommon]/degs[[j]][[1]][incommon])
+    }
+}
+
+colnames(corrs_sl)<-names(degs)
+rownames(corrs_sl)<-names(degs)
+colnames(corrs)<-names(degs)
+rownames(corrs)<-names(degs)
+
+df<-data.frame(R=c(corrs_sl[upper.tri(corrs_sl)],corrs[upper.tri(corrs)]),
+               method=c(rep("selfloops", length=length(corrs_sl[upper.tri(corrs_sl)])),
+                        rep("netdiff", length=length(corrs[upper.tri(corrs)]))))
+
+pdf("results/Compare_corr_kRatio_stroma.pdf", 4, 6)
+my_comparisons<-list(c("selfloops", "netdiff"))
+p<-ggviolin(df, x = "method", y = "R",
+            palette = "jco", add="boxplot") + stat_compare_means(comparisons=my_comparisons) +
+    rotate_x_text(angle = 45)
+print(p)
+dev.off()
+
+
+paletteLength <- 50
+myColor <- colorRampPalette(c("white", "red"))(paletteLength)
+myBreaks <- c(seq(0,max(unlist(corrs_sl), unlist(corrs), na.rm=T),
+                  length.out=floor(paletteLength)))
+length(myBreaks) == length(paletteLength) + 1
+
+p<-pheatmap(corrs_sl,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, "results/corr_kRatio_stroma_selfloops.pdf")
+
+p<-pheatmap(corrs,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, "results/ccorr_kRatio_stroma_selfloops.pdf")
+
+##################
+
+corrs <- matrix(nrow = length(degs_netdiff), ncol = length(degs_netdiff))
+for (i in 1:length(degs_netdiff)) {
+    for (j in 1:length(degs_netdiff)) {
+        incommon <-
+            intersect(names(degs_netdiff[[i]][[4]]),
+                      names(degs_netdiff[[j]][[4]]))
+        corrs[i, j] <- cor(degs_netdiff[[i]][[4]][incommon]/degs_netdiff[[i]][[2]][incommon],
+                           degs_netdiff[[j]][[4]][incommon]/degs_netdiff[[j]][[2]][incommon])
+    }
+}
+
+
+corrs_sl <- matrix(nrow = length(degs), ncol = length(degs))
+for (i in 1:length(degs)) {
+    for (j in 1:length(degs)) {
+        incommon <-
+            intersect(names(degs[[i]][[4]]),
+                      names(degs[[j]][[4]]))
+        corrs_sl[i, j] <- cor(degs[[i]][[4]][incommon]/degs[[i]][[2]][incommon],
+                              degs[[j]][[4]][incommon]/degs[[j]][[2]][incommon])
+    }
+}
+
+colnames(corrs_sl)<-names(degs)
+rownames(corrs_sl)<-names(degs)
+colnames(corrs)<-names(degs)
+rownames(corrs)<-names(degs)
+
+df<-data.frame(R=c(corrs_sl[upper.tri(corrs_sl)],corrs[upper.tri(corrs)]),
+               method=c(rep("selfloops", length=length(corrs_sl[upper.tri(corrs_sl)])),
+                        rep("netdiff", length=length(corrs[upper.tri(corrs)]))))
+
+pdf("results/Compare_corr_kRatio_epi.pdf", 4, 6)
+my_comparisons<-list(c("selfloops", "netdiff"))
+p<-ggviolin(df, x = "method", y = "R",
+            palette = "jco", add="boxplot") + stat_compare_means(comparisons=my_comparisons) +
+    rotate_x_text(angle = 45)
+print(p)
+dev.off()
+
+
+paletteLength <- 50
+myColor <- colorRampPalette(c("white", "red"))(paletteLength)
+myBreaks <- c(seq(0,max(unlist(corrs_sl), unlist(corrs), na.rm=T),
+                  length.out=floor(paletteLength)))
+length(myBreaks) == length(paletteLength) + 1
+
+p<-pheatmap(corrs_sl,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, "results/corr_kRatio_epi_selfloops.pdf")
+
+p<-pheatmap(corrs,   cellwidth=10, cellheight=10, breaks=myBreaks, color = myColor)
+save_pheatmap_pdf(p, "results/ccorr_kRatio_epi_selfloops.pdf")
+
+
+
